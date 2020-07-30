ee0e5aa97c72c9e4198237c3918e2e17c1637462
davotrey <davotrey@gmail.com>
2020-07-30 17:43:56 -0600

    added zeroing/init homing and more comments

--- test.py
+++ test.py
@@ -30,6 +30,7 @@ from ctypes.wintypes import (
 
 from thorlabs_kinesis import benchtop_piezo as bp
 
+# clears the keyboard buffer
 def flush_input():
     try:
         import msvcrt
@@ -38,29 +39,31 @@ def flush_input():
     except ImportError:
         import sys, termios
         termios.tcflush(sys.stdin, termios.TCIOFLUSH)
-
+# to prevent multiple prints/actions per key press
 def keyDebounce():
-    time.sleep(0.2) # sleep 200 ms to prevent multiple prints/actions per key press
+    time.sleep(0.2) # sleep 200 ms 
 
+# Home the stages at half-of-travel so they can be used to do slight adjustments
 def homeStages():
-    # Home the y and z directions at half-of-travel so they can be used to do slight adjustments
-    halfway = c_short(16383)
-    quarter = c_short(8191)
-    bp.PBC_SetPosition(serialno,bp.Channel1,halfway)
+    halfway = c_short(16383)    # half of travel 
+    bp.PBC_SetPosition(serialno,bp.Channel1,halfway)    # set the position
     bp.PBC_SetPosition(serialno,bp.Channel2,halfway)
     bp.PBC_SetPosition(serialno,bp.Channel3,halfway)
-    print("Stages Homing, waiting 5 sec to settle...")
-    time.sleep(5)
+    print("Stages Homing, waiting 3 sec to settle...")  # in the case of zeroing, there may be some settle-time
+    time.sleep(3)
     print("done")
 
+# increase the position of a provided channel by a certain joglength
 def upShift(jogLength,channel):
     currentPos = int(bp.PBC_GetPosition(serialno,channel))
     bp.PBC_SetPosition(serialno,channel,c_short(currentPos + jogLength))
 
+# decrease the position of a provided channel by a certain joglength
 def downShift(jogLength,channel):
     currentPos = int(bp.PBC_GetPosition(serialno,channel))
     bp.PBC_SetPosition(serialno,channel,c_short(currentPos - jogLength))
 
+# allows for more direct control of the x channel movement
 def move():
     print("Press 'w' to shift 10 nm forward\nPress 's' to shift 80 nm backward\nPress 'e' to shift 5 nm forward\nPress 'd' to shift 5nm backward\nPress 'q' to exit function")
     Done = False
@@ -68,8 +71,8 @@ def move():
     medium = int(16)        # 10 nm
     small = int(8)          # 5 nm
     extraLarge = int(200)   # 125 nm
-    # test
 
+    # look for key presses, q reverts to main menu
     while not Done:
         try:
             if keyboard.is_pressed('q'):
@@ -97,6 +100,10 @@ def move():
         except:
             continue
 
+# for coupling a resonator to a stationary tapered fiber
+# draws the resonator closer (x channel/axis) at a variable rate
+# stopping the movement draws back and enters the move state
+# for more fine control of the coupling, good for coupling at a gap
 def couple():
     print("Press 'w' to increase jog step forward (accelerate)\nPress 's' to decrease jog step forward (decelerate)\nPress 'q' to exit function\n\nStarted Motion towards Fiber...")
     jogLength = int(16) # just under 10 nm of travel
@@ -130,6 +137,7 @@ def couple():
         printCounter += 1 
         time.sleep(0.2)
 
+# allows for rough adjustment of the y and z channels
 def adjust(channel):
     print("Press 'w' to shift 0.5 um up or right\nPress 's' to shift 0.5 um down or left\nPress 'q' to exit function")
     jogLength = 800 # about half a micron out of 20 um of 
@@ -160,9 +168,9 @@ def adjust(channel):
 
 
 maxTravel = 32767 # travel is from 0 to 32767 for 20 um
-if bp.TLI_BuildDeviceList() == 0:
+if bp.TLI_BuildDeviceList() == 0:   # if there are devices properly built
     print("Device list built (no errors).")
-    size = bp.TLI_GetDeviceListSize()
+    size = bp.TLI_GetDeviceListSize()   # finds the amount of devices seen
     print(size, "device(s) found.")
     if size > 0:    # if at least one thor labs device is found
 
@@ -179,21 +187,29 @@ if bp.TLI_BuildDeviceList() == 0:
 
         # x axis
         bp.PBC_EnableChannel(serialno, bp.Channel1) # enables control of specified channel
-        bp.PBC_SetPositionControlMode(serialno, bp.Channel1, bp.CTR_ClosedLoop) # set the channel to closed loop
-        bp.PBC_Identify(serialno, bp.Channel1)  # cause the LED readout to blink to identify it
+        bp.PBC_SetZero(serialno, bp.Channel1)   # set the voltage to 0, then define that position as 0
+        #bp.PBC_Identify(serialno, bp.Channel1)  # cause the LED readout to blink to identify it
+        print("zeroing channel 1")
+
         # y axis
         bp.PBC_EnableChannel(serialno, bp.Channel2)
-        bp.PBC_SetPositionControlMode(serialno, bp.Channel2, bp.CTR_ClosedLoop)
+        bp.PBC_SetZero(serialno, bp.Channel2)   # set the voltage to 0, then define that position as 0
         # bp.PBC_Identify(serialno, bp.Channel2)
+        print("zeroing channel 2")
+
         # z axis
         bp.PBC_EnableChannel(serialno, bp.Channel3)
-        bp.PBC_SetPositionControlMode(serialno, bp.Channel3, bp.CTR_ClosedLoop)
+        bp.PBC_SetZero(serialno, bp.Channel3)   # set the voltage to 0, then define that position as 0
         # bp.PBC_Identify(serialno, bp.Channel3)
+        print("zeroing channel 3")
+        time.sleep(40)
 
-        #Identify Control Mode TODO: remove this code after testing
-        print("control type for bp.Channel3: ", bp.PBC_GetPositionControlMode(serialno,bp.Channel1))
+        #Identify Control Mode TODO: remove this code after testing 
+        print("control type for bp.Channel1: ", bp.PBC_GetPositionControlMode(serialno,bp.Channel1))
         print("control type for bp.Channel2: ", bp.PBC_GetPositionControlMode(serialno,bp.Channel2))
         print("control type for bp.Channel3: ", bp.PBC_GetPositionControlMode(serialno,bp.Channel3))
+
+        homeStages()
 
         Done = False
         while not Done:
@@ -209,7 +225,7 @@ if bp.TLI_BuildDeviceList() == 0:
             elif command is 'y':
                 adjust(bp.Channel2)
             elif command is '?':
-                print('\nh -------- home the three axis\nc -------- couple (q to quit, w to speed up, s to slow down\nm -------- move x axis at different step sizes\nz -------- adjust z axis (q to quit, w to move up, s to move down\ny -------- adjust y axis (q to quit, w to move up, s to move down\nq -------- quit')
+                print('\nh -------- home the three axis\nc -------- couple (q to quit, w to speed up, s to slow down)\nm -------- move x axis at different step sizes\nz -------- adjust z axis (q to quit, w to move up, s to move down)\ny -------- adjust y axis (q to quit, w to move up, s to move down)\nq -------- quit')
             elif command is 'q':
                 Done = True
 
@@ -218,6 +234,6 @@ if bp.TLI_BuildDeviceList() == 0:
         bp.PBC_StopPolling(serialno,bp.Channel2)
         bp.PBC_StopPolling(serialno,bp.Channel3)
         if bp.PBC_Disconnect(serialno) == 0:
-                print("device disconnected")
+            print("device disconnected")
         bp.PBC_Close(serialno)
         flush_input()
